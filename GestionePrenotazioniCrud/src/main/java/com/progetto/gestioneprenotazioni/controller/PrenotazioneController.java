ef3b20a3186249d090b1ae93b826fa632c287863
diclementef92 <diclementef92@gmail.com>
2023-04-26 15:54:04 +0100

    put prenotazioni e collection postman

--- GestionePrenotazioniCrud/src/main/java/com/progetto/gestioneprenotazioni/controller/PrenotazioneController.java
+++ GestionePrenotazioniCrud/src/main/java/com/progetto/gestioneprenotazioni/controller/PrenotazioneController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -44,6 +45,16 @@ public class PrenotazioneController {
 
 		try {
 			return new ResponseEntity<Prenotazione>(prenotazioneService.insertPrenotazione(p), HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_MODIFIED);
+		}
+	}
+
+	@PutMapping
+	public ResponseEntity<?> modificaPrenotazione(@RequestBody Prenotazione p) {
+
+		try {
+			return new ResponseEntity<Prenotazione>(prenotazioneService.updatePrenotazione(p), HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_MODIFIED);
 		}
