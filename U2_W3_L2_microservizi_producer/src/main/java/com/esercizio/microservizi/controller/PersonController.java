8bc0bc016ab53e45c5d1ae37b269a56e3ca3f8ad
diclementef92 <diclementef92@gmail.com>
2023-04-26 20:14:51 +0100

    aggiunti metodi get data 1 2 e 3

--- U2_W3_L2_microservizi_producer/src/main/java/com/esercizio/microservizi/controller/PersonController.java
+++ U2_W3_L2_microservizi_producer/src/main/java/com/esercizio/microservizi/controller/PersonController.java
@@ -1,11 +1,15 @@
 package com.esercizio.microservizi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.esercizio.microservizi.model.Person;
 import com.esercizio.microservizi.service.PersonService;
 
 @RestController
@@ -17,8 +21,16 @@ public class PersonController {
 
 	@GetMapping("/data1")
 	public ResponseEntity<String> testResponseString() {
-		return "stringa di esempio da data1";
-
+		return new ResponseEntity<String>("stringa di esempio da data1", HttpStatus.OK);
 	}
 
+	@GetMapping("/data2")
+	public ResponseEntity<List<Person>> testResponseListofFourPersons() {
+		return new ResponseEntity<List<Person>>(personService.getAllPersons().subList(0, 4), HttpStatus.OK);
+	}
+
+	@GetMapping("/data3")
+	public ResponseEntity<List<Person>> testResponseListofFourPersonsJSON() {
+		return new ResponseEntity<List<Person>>(personService.getAllPersons().subList(0, 4), HttpStatus.OK);
+	}
 }
